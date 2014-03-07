c75bc1c255f58c7c3fd8fe9dc566e989e3163f42
Daniel Kessler <sinani201@gmail.com>
2014-03-06 18:10:19 -0800

    is now properly packaged

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Basic Forum Software to Practice Webdev
-This is a very basic forum software. It supports making threads and posting to threads. You probably shouldn't use it for anything, but it *does* work.
+# Basic Forum Software To Practice Webdev
+This is a very basic forum software. It supports making threads and posting to threads.
 
 Currently it does not have any protection against fast posting/spamming.
 
 All posts can use markdown. Some elements (like h1) may render strangely.
 
-To use it, just add it to your django apps. It's that easy! The only depedency is python markdown.
+Check out a live demo at [nisani.bot.nu/bfstpw](http://nisani.bot.nu/bfstpw/)!
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="django-bfstpw",
+    version="1.0",
+    packages=["bfstpw"],
+    include_package_data=True,
+    install_requires=["markdown"],
+    license="MIT License",
+    description="Basic forum software",
+    author="Daniel Kessler",
+    author_email="sinani201@gmail.com",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Communications :: BBS",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards",
+    ]
+)
